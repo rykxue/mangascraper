c3a2b071b28a34d4a7b551e9c24830aca2efc1b4
rykxue <rykxue.0fx7@gmail.com>
2025-01-06 05:35:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -16,10 +16,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/error.html');
-});
-
 app.use('/images', (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
